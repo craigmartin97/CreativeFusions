900457296d7df47cf43457c13164ab4e285e72b6
craigmartin97 <c.martin0297@gmail.com>
2020-12-26 14:48:27 +0000

    Add project files.

---
+++ CreativeFusions.Auth/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CreativeFusions.Auth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
